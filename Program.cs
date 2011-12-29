215c08d32ba47da874a724f578d33df0057a40c2
Sergey Korenkov <3ug3ag@gmail.com>
2011-12-29 17:23:14 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BBeguTe Ko/lu4ecTBo 6ykB:");
+            string chislo = Console.ReadLine();
+            int length = int.Parse(chislo);
+            
+            string Chars = "qwerty";
+            string RESULT = "";
+
+            for (int j = 0; j <= length-1; j++)
+            {System.Random number = new System.Random((int)DateTime.Now.Ticks);
+             int rnd = number.Next(4);
+             RESULT += Convert.ToChar(Chars[rnd]);}
+
+            Console.WriteLine ("{0}" , RESULT);
+            Console.ReadLine ();
+
+
+        }
+    } 
+}
