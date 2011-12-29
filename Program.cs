19fcd9dccfc30bab509a920ab5e667fc84730b44
Sergey Korenkov <3ug3ag@gmail.com>
2011-12-29 18:16:25 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -8,21 +8,18 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("BBeguTe Ko/lu4ecTBo 6ykB:");
-            string chislo = Console.ReadLine();
-            int length = int.Parse(chislo);
-            
-            string Chars = "qwerty";
+        {}
+            static string getrandom(int length)        
+    {          
+            string Chars = "qwertyuiopasdfghjklzxcvbnm";
             string RESULT = "";
 
             for (int j = 0; j <= length-1; j++)
             {System.Random number = new System.Random((int)DateTime.Now.Ticks);
-             int rnd = number.Next(4);
+             int rnd = number.Next(25);
              RESULT += Convert.ToChar(Chars[rnd]);}
 
-            Console.WriteLine ("{0}" , RESULT);
-            Console.ReadLine ();
+            return RESULT;
 
 
         }
