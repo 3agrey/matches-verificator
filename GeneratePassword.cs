b2e108b8fa30b4f2a962018848c3356fd8f530ed
unknown <Admin@.(none)>
2012-01-09 19:46:49 +0200

    new work space

---
+++ GeneratePassword.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneratePassword
+{
+    public class RandomPassword
+    {
+        int length = 10;
+
+        public static string Generate(int length)
+        {
+            Random rnd = new Random();
+            char[] res = null;
+            for (int ctr = 0; ctr <= length; ctr++)
+            {
+                res= new char[rnd.Next(65,91)];
+            }
+            return new string(res);
+        }
+    }
+}
