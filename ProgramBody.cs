93e882dac347c301d6856005b82ff37cdf17c58e
3agrey <3ug3ag@gmail.com>
2012-01-18 22:24:15 +0200

    separated to two pieces

---
+++ ProgramBody.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program
+{
+    class Body
+    {
+        public class Start 
+        {
+            static void Main(string[] args)
+            {
+                string password = Function.RandomChars.Generate(10);
+                System.Console.WriteLine(password);
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
