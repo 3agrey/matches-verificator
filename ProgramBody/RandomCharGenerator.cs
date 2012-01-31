2a80853e9ebcfd0e52d17b1e01fd7195a2581143
3agrey <3ug3ag@gmail.com>
2012-02-01 00:44:46 +0200

    new clear project

---
+++ ProgramBody/RandomCharGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ppc.GeneratePassword
+{
+    public class RandomChars
+    {
+        //todo: make an isntanse method, not static
+        public static string Generate(int length)
+        {
+            Random rnd = new Random();
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                char tempchar = (Convert.ToChar(rnd.Next(97, 122)));
+                res.Append(tempchar);
+            }
+            string result = res.ToString();
+            return result;
+        }
+    }
+}
---
+++ RandomCharGeneratorTest/RandomCharsTest.cs
@@ -0,0 +1,93 @@
+﻿using Ppc.GeneratePassword;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RandomCharsTest
+{
+
+
+    /// <summary>
+    ///Это класс теста для RandomCharsTest, в котором должны
+    ///находиться все модульные тесты RandomCharsTest
+    ///</summary>
+    [TestClass()]
+    public class RandomCharsTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты теста
+        // 
+        //При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        //ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //TestInitialize используется для выполнения кода перед запуском каждого теста
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //TestCleanup используется для выполнения кода после завершения каждого теста
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Тест для Generate
+        ///</summary>
+        [TestMethod()]
+        public void GenerateTest()
+        {
+            int length = 0;
+            string actual;
+            string str = string.Empty;
+            actual = RandomChars.Generate(length);
+            Assert.AreEqual<Int32>(0, actual.Length, ("not equal"));
+
+            length = 1;
+            actual = RandomChars.Generate(length);
+            Assert.AreEqual<Int32>(1, actual.Length, ("not equal"));
+
+            length = 15;
+            actual = RandomChars.Generate(length);
+            Assert.AreEqual<Int32>(15, actual.Length, ("not equal"));
+
+            length = 1024;
+            actual = RandomChars.Generate(length);
+            Assert.AreEqual<Int32>(1024, actual.Length, ("not equal"));
+
+        }
+    }
+}
